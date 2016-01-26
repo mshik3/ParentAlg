2d4c13992551a09cb07c24a2702a51dc2ea05eaa
Brian Thompson <brthomp2@illinois.edu>
2016-01-25 22:09:40 -0600

    added class status

--- PledgeSort.py
+++ PledgeSort.py
@@ -3,12 +3,74 @@ import math
 from random import shuffle
 
 #populate pledge list from CSV
-pledges = []
+freshmen = []
+sophomores = []
+juniors = []
+seniors = []
+
 with open('Database/CSV/example.csv', 'rb') as csvfile:
      reader = csv.reader(csvfile, delimiter=',', quotechar='|')
      for row in reader:
-         pledges.append(row)
-shuffle(pledges)
+        #pledges.append(row)
+        if "freshman" in row[0]:
+          freshmen.append(row)
+        elif "sophomore" in row[0]:
+          sophomores.append(row)
+        elif "junior" in row[0]:
+          juniors.append(row)
+        elif "senior" in row[0]:
+          seniors.append(row)
+
+shuffle(freshmen)
+shuffle(sophomores)
+shuffle(juniors)
+shuffle(seniors)
+
+pledges = [None] * (len(freshmen)+len(sophomores)+len(juniors)+len(seniors))*200
+length = (len(freshmen)+len(sophomores)+len(juniors)+len(seniors))*200
+
+
+
+print 'freshmen:',len(freshmen)
+print 'sophomores:',len(sophomores)
+print 'juniors:',len(juniors)
+print 'seniors:',len(seniors)
+
+
+i = 0
+if(len(freshmen)>0):
+  iterator = length/len(freshmen)
+  while(len(freshmen)>0):
+    if(pledges[i] is None):
+      pledges[i]=freshmen.pop()[0]
+    i+=iterator
+i = 4
+if(len(sophomores)>0):
+  iterator = length/len(sophomores)
+  while(len(sophomores)>0):
+    if(pledges[i] is None):
+      pledges[i]=sophomores.pop()[0]
+    i+=iterator
+
+i = 8
+if(len(juniors)>0):
+  iterator = length/len(juniors)
+  while(len(juniors)>0):
+    if(pledges[i] is None):
+      pledges[i]=juniors.pop()[0]
+    i+=iterator
+
+i = 12
+if(len(seniors)>0):
+  iterator = length/len(seniors)
+  while(len(seniors)>0):
+    if(pledges[i] is None):
+      pledges[i]=seniors.pop()[0]
+    i+=iterator
+
+pledges = filter(None, pledges)
+
+
 
 dynasties = ["Eagles","Torches","FMNs","Scouts","Diamonds","Oaks"]
 percentages = []
@@ -39,7 +101,7 @@ for i in range(len(dynasties)):
 	fo = open("Database/Pledges/"+dynasties[i]+".txt", "w+")
 	fo.write(dynasties[i]+": \n")
 	for index in range(len(results[i])):
-	   fo.write(results[i][index][0]+"\n")
+	   fo.write(results[i][index]+"\n")
 	fo.write("\n\n")
 	# Close opend file
 	fo.close()
