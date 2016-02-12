3a38179567f5ff335d33293f4449c95fe72611b3
Brian Thompson <brthomp2@illinois.edu>
2016-02-12 12:28:08 -0600

    Favored pledges preferences more

--- ParentMatching/ParentAlg.py
+++ ParentMatching/ParentAlg.py
@@ -6,29 +6,39 @@ results = {}
 
 pledges = {}
 parents = {}
+
+# Of the form [child, (parent, rankofparent, rankofchild)], Lower rank is better
 matched = {}
 
 def stableMatching():
 	for parent in parents:
 		#find first free child on parents list
-		for child in parents[parent]:
+		for j in range(len(parents[parent])):
 			# Child's rank of the parent
-			rank = 8
+			child = parents[parent][j]
+			rank = 6
 			for i in range(len(pledges[child])):
 				if parent == pledges[child][i]:
 					rank = i
 					break
 			if child not in matched.keys():
-				matched[child]=(parent,rank)
+				matched[child]=(parent,rank,j)
 			else:
-				if rank < matched[child][1]:
-					matched[child]=(parent,rank)
+				if (j+rank) < matched[child][1]+matched[child][1]:
+					matched[child]=(parent,rank,j)
+	print(matched)
+	for i in range(len(matched.keys())-1,-1,-1):
+		child = matched.keys()[i]
+		if matched[child][1]>=6:
+			del matched[child]
 	for key in matched:
 		results[matched[key][0]].append(key)
 		del pledges[key]
 		parents[matched[key][0]].remove(key)
-			
 
+	for pledge in reversed(pledges.keys()):
+		results[pledges[pledge][0]].append(pledge)
+		del pledges[pledge]
 
 with open('TestData/TestPledgeResults.csv', 'rb') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -50,7 +60,7 @@ for parent in parents:
 	results[parent]=[]
 print(len(pledges))
 stableMatching()
-print(len(pledges))
+print len(pledges) , '\n'
 
 for parent in results:
 	print parent,": ", results[parent]
